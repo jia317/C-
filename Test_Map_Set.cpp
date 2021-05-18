1ee216e25b39e130e173040c6b72778f2229d5c5
jia317 <67813941+jia317@users.noreply.github.com>
2021-05-18 21:16:57 +0800

    map和set实现的测试

---
+++ Test_Map_Set.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+#include"Map.h"
+#include"Set.h"
+
+int main()
+{
+	TestMap();
+
+	cout << "--------------------------------" << endl;
+
+	TestSet();
+
+	system("pause");
+	return 0;
+}
