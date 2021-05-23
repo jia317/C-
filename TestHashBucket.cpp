3c4ea418ff62ecacb213711cb52088a157277229
jia317 <67813941+jia317@users.noreply.github.com>
2021-05-23 15:53:37 +0800

    哈希结构的测试demo

---
+++ TestHashBucket.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+//#include"HashBucket.hpp"
+#include"UnorderedMap.hpp"
+#include"UnorderedSet.hpp"
+//#include"HashBucket_MS.hpp"
+
+// ²âÊÔ¹þÏ£Í°
+#if 0
+int main()
+{
+	//TestBucket1();
+	//TestBucket2();
+	//TestBucket3();
+	TestBucket4();
+
+	system("pause");
+	return 0;
+}
+#endif
+
+// ¼ì²â·â×°µÄunordered_map
+// ¼ì²â·â×°µÄunordered_set
+#if 1
+int main()
+{
+	//TestUnorderedMap();
+	TestUnorderedSet();
+
+	system("pause");
+	return 0;
+}
+#endif
+
+#if 0
+int main()
+{
+	//TestBucket3();
+	//TestCheckCap();
+
+	system("pause");
+	return 0;
+}
+#endif
+
+
